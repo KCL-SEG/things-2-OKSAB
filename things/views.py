c2fa123efab14f1bd8c4e11c9b57de775aeb653e
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-11-13 00:04:48 +0000

    Add solution

--- things/views.py
+++ things/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
+from .forms import ThingForm
 
-def home(request):
+def home_view(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect after POST
+    else:
+        form = ThingForm()  # An unbound form
+
     return render(request, 'home.html', {'form': form})
